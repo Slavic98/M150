7dbe3ca56df4902aa926bfd2604e230c257f3e3e
Veaceslav Salcov1 <Veaceslav_Salcov1@sluz.ch>
2019-12-17 18:02:09 +0100

    Projektdateien hinzufügen.

---
+++ Turnierverwaltung/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Turnierverwaltung.Startup))]
+namespace Turnierverwaltung
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
